3e306c27f21f0dfe8e319e5935b77404a3762dc0
perpetuumm0bi1e <kat.kus@mail.ru>
2023-04-02 23:43:31 +0300

    the last version

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ let firstWorkStatisticsDay = document.getElementById('firstWorkStatisticsDay');
 let firstWorkStatisticsWeek = document.getElementById('firstWorkStatisticsWeek');
 let firstWorkStatisticsCourse = document.getElementById('firstWorkStatisticsCourse');
 
-for(let i = 0; i < firstWorkCards.length; i++){
-    firstWorkCards[i].onmouseover  = function() {
-        for(let j = i; j < firstWorkCards.length; j++){
-            firstWorkCards[j].style.transform = 'rotate(5deg)';
+for (let i = 0; i < firstWorkCards.length; i++) {
+    firstWorkCards[i].onmouseover = function() {
+
+        for (let j = 0; j < firstWorkCards.length; j++) {
             firstWorkCards[j].style.opacity = '.4';
             firstWorkCards[j].style.zIndex = '0';
-        }
-        for(let j = 0; j < i; j++){
-            firstWorkCards[j].style.transform = 'rotate(-5deg)';
-            firstWorkCards[j].style.opacity = '.4';
-            firstWorkCards[j].style.zIndex = '0';
+            if (j < i) {
+                firstWorkCards[j].style.transform = 'rotate(-5deg)';
+            } else if (j > i) {
+                firstWorkCards[j].style.transform = 'rotate(5deg)';
+            }
         }
         firstWorkCards[i].style.transform = 'scale(1.5)';
         firstWorkCards[i].style.opacity = '1';
-        firstWorkCards[i].style.zIndex = '2';
+        firstWorkCards[i].style.zIndex = '1';
     }
-    firstWorkCards[i].onmouseleave  = function() {
-        for(let j = 0; j < firstWorkCards.length; j++){
+    firstWorkCards[i].onmouseleave = function() {
+        for (let j = 0; j < firstWorkCards.length; j++) {
             firstWorkCards[j].style.transform = 'rotate(0deg)';
             firstWorkCards[j].style.transform = 'scale(1)';
             firstWorkCards[j].style.opacity = '1';
