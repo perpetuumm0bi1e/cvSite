52393b3461aa168e98769552bae936e2dbcfd59d
perpetuumm0bi1e <kat.kus@mail.ru>
2023-04-03 15:37:43 +0300

    the last version

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ for (let i = 0; i < firstWorkCards.length; i++) {
 
         for (let j = 0; j < firstWorkCards.length; j++) {
             firstWorkCards[j].style.opacity = '.4';
-            firstWorkCards[j].style.zIndex = '0';
+            firstWorkCards[j].style.zIndex = "\u0027" + String(`${j}`) + "\u0027";
             if (j < i) {
                 firstWorkCards[j].style.transform = 'rotate(-5deg)';
             } else if (j > i) {
@@ -20,7 +20,7 @@ for (let i = 0; i < firstWorkCards.length; i++) {
         }
         firstWorkCards[i].style.transform = 'scale(1.5)';
         firstWorkCards[i].style.opacity = '1';
-        firstWorkCards[i].style.zIndex = '1';
+        firstWorkCards[i].style.zIndex = '6';
     }
     firstWorkCards[i].onmouseleave = function() {
         for (let j = 0; j < firstWorkCards.length; j++) {
